f657caafbb00bad9820054f107a3a10b43ee860b
praveen choudhary <pc852180@gmail.com>
2025-01-30 23:30:50 +0530

    props and map method in react

--- PropsReactProject3/src/App.jsx
+++ PropsReactProject3/src/App.jsx
@@ -1,11 +1,24 @@
 
 import './App.css'
+import Studata from './StudentDatabase'
+import StudentStyle from './StudentStyling'
+const ans = Studata.map((key)=><StudentStyle  rollno={key.rollnumber} name={key.name} sub={key.subject} fs={key.fees}  />)
 
 function App() {
  
 
   return (
     <>
+    <h1 align="center">Welcome to my home screen</h1>
+    <table align='center' border={1} bgcolor='yellow'>
+      <tr>
+        <th>Rollnumber</th>
+        <th>Name</th>
+        <th>Subject</th>
+        <th>Fees</th>
+      </tr>
+      {ans}
+    </table>
      
     </>
   )

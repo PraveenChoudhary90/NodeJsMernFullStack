39d4ce515c7e6a08906e436715b336ff02d7eb17
praveen choudhary <pc852180@gmail.com>
2025-02-14 11:43:13 +0530

    Todo list Redux toolkit

--- RedexTodoList/src/App.jsx
+++ RedexTodoList/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import {useSelector, useDispatch} from "react-redux";
-import { MyAdd } from "./TodoSlice";
+import { MyAdd , Mydelete,Complete, InComplete, MyEditDataSave} from "./TodoSlice";
 import Table from 'react-bootstrap/Table';
 
 
@@ -8,15 +8,69 @@ function App(){
   const [val,setVal]= useState("");
   const MyTaskbtn = useSelector(state=>state.todo.task);
   const dispatch = useDispatch();
+const [btnStatus,setbtnStatus]=useState(true);
+const [workid, setWorkId] = useState("");
+
+  const myComplete=(id)=>{
+    dispatch(Complete({id:id}));
+  }
+  const myInComplete=(id)=>{
+    dispatch(InComplete({id:id}));
+  };
+
+
+ const MyEditdata = ()=>{
+    setVal(work);
+    setWorkId(id);
+    setbtnStatus(flase)
+ }
+
+
+
+ const Myeditdatasave = ()=>{
+  dispatch(MyEditDataSave({id:workid, data:val}));
+  setbtnStatus(true);
+ }
+
+
+
+
+
 
  let sno =0;
-  const ans  =MyTaskbtn.map((key)=>{
+  const ans  =MyTaskbtn.map((key,index)=>{
        sno++;
        return(
         <>
-        <tr>
+        <tr key={index}>
           <th>{sno}</th>
-          <td>{key.work}</td>
+          <td>
+             {key.CompStatus ? 
+             (<> 
+                  
+                   <span style={{color:"red", textDecoration:"line-through"}}> {key.work}  </span> 
+                  </>) :
+                   (<> 
+                  
+                    {key.work}
+                  </>)}
+          </td>
+          <td onClick={()=>{dispatch(Mydelete({id:key.id}))}} >Delete</td>
+          <td>
+            {key.ComStatus?(
+              <>
+          <td onClick={()=>{myInComplete(key.id)}}>InComplete</td>
+
+              </>
+            ):(
+              <>
+          <td onClick={()=>{myComplete(key.id)}}>Complete</td>
+              
+              
+              </>
+            )}
+          </td>
+          <td onClick={()=>{Myeditdatasave(key.id,key.work)}}>Edit</td>
         </tr>
         
         </>
@@ -24,22 +78,34 @@ function App(){
   })
   return(
     <>
+    <center>
     <h1> Welcome to our todo list</h1>
     Enter your task :<input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}}/>
-    <button onClick={()=>{dispatch(MyAdd({work:val}))}}>Add</button>
+    {btnStatus?
+    (<>
+    
+    <button onClick={()=>{dispatch(MyAdd({id:Date.now(),work:val, ComStatus:false}))}}>Add</button>
+    
+    </>):
+    (<>
+    
+    <button onClick={()=>{MyEditdata(key.id,key.work)}}>Edit</button>
+    
+    </>)}
 
     <hr size="6" color="red" />
      <Table striped bordered hover style={{width:"50%"}}>
       <thead>
         <tr>
           <th>#</th>
-          <th>First Name</th>
+          <th>Add Task Here</th>
         </tr>
       </thead>
       <tbody>
        {ans}
       </tbody>
       </Table>
+      </center>
     </>
   )
 
--- RedexTodoList/src/TodoSlice.jsx
+++ RedexTodoList/src/TodoSlice.jsx
@@ -9,9 +9,38 @@ const MyTodo  = createSlice({
         MyAdd :(state, actions)=>{
             console.log(actions.payload)
             state.task.push(actions.payload)
+        },
+        Mydelete:(state, actions)=>{
+            state.task = state.task.filter(key=>key.id!=actions.payload.id)
+        },
+        Complete:(state,actions)=>{
+            for(let i=0;i<state.task.length;i++)
+                {
+                if(state.task[i]==actions.payload.id){
+                    state.task[i].ComStatus=true
+                }
+            }
+        },
+         InComplete:(state,actions)=>{
+            for(let i=0;i<state.task.length;i++)
+                {
+                if(state.task[i].id==actions.payload.id){
+                   state.task[i].ComStatus=false
+                }
+            }
+        },
+        MyEditDataSave:(state,actions)=>{
+              for(var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].work=actions.payload.data;
+                }
+            }
+            
         }
 
     }
 })
-export const {MyAdd}=MyTodo.actions;
+export const {MyAdd , Mydelete,Complete,InComplete , MyEditDataSave}=MyTodo.actions;
 export default MyTodo.reducer;

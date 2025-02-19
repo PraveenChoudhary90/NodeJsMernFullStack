fe6f4a5e41e322b1b669579b958288a882dac2bb
praveen choudhary <pc852180@gmail.com>
2025-02-19 13:19:15 +0530

    Redux todolist

--- ReduxMapTodolist/src/App.jsx
+++ ReduxMapTodolist/src/App.jsx
@@ -1,64 +1,93 @@
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import { useState } from 'react'
-import './App.css'
-import { Myaddtask, DeleteTask, Myeditdata } from './TodoSlice';
-import {useSelector, useDispatch} from "react-redux"
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import "./App.css";
+import { Myaddtask, DeleteTask, Myeditdata } from "./TodoSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   const [input, setinput] = useState("");
-  const mydata = useSelector(state=>state.todolist.task);
+  const mydata = useSelector((state) => state.todolist.task);
   const dispatch = useDispatch();
   const [workId, setWorkId] = useState("");
-  console.log(mydata)
+  const [editinput, seteditinput] = useState("");
+  // console.log(mydata)
 
-  const EditTask =(id,work)=>{
+  const EditTask = (id, work) => {
     setinput(work);
     setWorkId(id);
-  }
-
-     const myEditSave = () => {
-    dispatch(Myeditdata({id: workId, data:input }));
-    
   };
- 
- let sno = 0;
-   const ans = mydata.map((key, index)=>{
+
+  const myEditSave = () => {
+    console.log(input);
+    seteditinput(input);
+    dispatch(Myeditdata({ id: workId, data: input}));
+  };
+
+  let sno = 0;
+  const ans = mydata.map((key, index) => {
     sno++;
-    return(
+    return (
       <>
         <tr key={index}>
           <td>{sno}</td>
           <td>{key.work}</td>
-          <td onClick={()=>{dispatch(DeleteTask({id:key.id}))}}><Button variant="danger">Delete</Button></td>
-          <td onClick={()=>{EditTask(key.id, key.work)}}><Button variant="primary">Update</Button></td>
+          <td
+            onClick={() => {
+              dispatch(DeleteTask({ id: key.id }));
+            }}
+          >
+            <Button variant="danger">Delete</Button>
+          </td>
+          <td
+            onClick={() => {
+              EditTask(key.id, key.work);
+            }}
+          >
+            <Button variant="primary">Update</Button>
+          </td>
         </tr>
-     
       </>
-    )
-   })
+    );
+  });
 
   return (
     <>
-     <h1>Welcome to our todolist</h1>
-     Enter the Task: <input type='text' value={input} onChange={(e)=>{setinput(e.target.value)}}/>
-     <Button variant="success" onClick={()=>{dispatch(Myaddtask({id:Date.now(),work:input,}))}}>Click Add</Button>
-     <Button variant="info" onClick={myEditSave} > Click Edit</Button>
-     <hr size="6" color='red' />
+      <h1>Welcome to our todolist</h1>
+      Enter the Task:{" "}
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => {
+          setinput(e.target.value);
+        }}
+      />
+      <Button
+        variant="success"
+        onClick={() => {
+          dispatch(Myaddtask({ id: Date.now(), work: input }));
+        }}
+      >
+        Click Add
+      </Button>
+      <Button variant="info" onClick={myEditSave}>
+        {" "}
+        Click Edit
+      </Button>
+      <hr size="6" color="red" />
       <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Serial Number</th>
-          <th>Add Task here</th>
-          <th>Delete</th>
-          <th>Update</th>
-        </tr>
-         {ans}
-      </thead>
+        <thead>
+          <tr>
+            <th>Serial Number</th>
+            <th>Add Task here</th>
+            <th>Delete</th>
+            <th>Update</th>
+          </tr>
+          {ans}
+        </thead>
       </Table>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- ReduxMapTodolist/src/TodoSlice.jsx
+++ ReduxMapTodolist/src/TodoSlice.jsx
@@ -1,31 +1,28 @@
-import {createSlice} from "@reduxjs/toolkit";
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const TodoSlice = createSlice({
-    name:"todolist",
-    initialState:{
-        task:[]
+  name: "todolist",
+  initialState: {
+    task: [],
+  },
+  reducers: {
+    Myaddtask: (state, actions) => {
+      console.log(actions.payload);
+      state.task.push(actions.payload);
     },
-    reducers:{
-        Myaddtask:(state,actions)=>{
-            console.log(actions.payload);
-            state.task.push(actions.payload);
-        },
-        DeleteTask:(state,actions)=>{
-            state.task = state.task.filter(key=>key.id!=actions.payload.id);
-
-        },
-        Myeditdata:(state,actions)=>{
-            for(let i = 0; i<state.task.length;i++){
-
-                if(state.task[i].id==actions.payload.id)
-                {
-                   state.task[i].work=state.task.data
-                }
-            }
+    DeleteTask: (state, actions) => {
+      state.task = state.task.filter((key) => key.id != actions.payload.id);
+    },
+    Myeditdata: (state, actions) => {
+      console.log(actions);
+      for (let i = 0; i < state.task.length; i++) {
+        if (state.task[i].id == actions.payload.id) {
+          state.task[i].work = actions.payload.data
         }
-    }
-})
+      }
+    },
+  },
+});
 
-export const {Myaddtask,DeleteTask,Myeditdata} = TodoSlice.actions;
+export const { Myaddtask, DeleteTask, Myeditdata } = TodoSlice.actions;
 export default TodoSlice.reducer;
